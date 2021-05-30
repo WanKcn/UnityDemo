308dfeaa57e439a10789013f8483430b38422188
wankcn <wankcn@icloud.com>
2021-05-30 16:49:22 +0800

    修该文件夹，增加readme

---
+++ ECSHelloWorld/ECSHelloWorld2018VER/Assets/Scripts/RotationCubeComponent.cs
@@ -0,0 +1,15 @@
+﻿/***
+ *  使用Unity2018.4版本 开发ECS的HelloWorld
+ *
+ *  定义ECS的"组件"  组件只有数据 没有方法
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationCubeComponent : MonoBehaviour
+{
+    /// 旋转速度
+    public float speed;
+}
---
+++ ECSHelloWorld/ECSHelloWorld2018VER/Assets/Scripts/RotationCubeSystem.cs
@@ -0,0 +1,30 @@
+﻿/***
+ *  使用Unity2018.4版本 开发ECS的HelloWorld
+ *
+ *  定义ECS的"系统" 只有方法没有数据
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities; // 引入命名空间
+
+// 抽象方法
+public class RotationCubeSystem : ComponentSystem
+{
+    struct Group
+    {
+        public RotationCubeComponent rotation;
+        public Transform transform;
+    }
+
+    protected override void OnUpdate()
+    {
+        // GetEntities 2018所有 2019被废弃
+        foreach (var en in GetEntities<Group>())
+        {
+            // 进行旋转
+            en.transform.Rotate(Vector3.down, en.rotation.speed * Time.deltaTime);
+        }
+    }
+}
