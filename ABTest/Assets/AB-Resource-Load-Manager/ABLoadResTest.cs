4bfc1268c97d20758afba7d0ad8fc7248411da9c
wankcn <wankcn@icloud.com>
2021-06-04 19:59:11 +0800

    异步加载

--- ABTest/Assets/AB-Resource-Load-Manager/ABLoadResTest.cs
+++ ABTest/Assets/AB-Resource-Load-Manager/ABLoadResTest.cs
@@ -7,12 +7,21 @@ public class ABLoadResTest : MonoBehaviour
 {
     private void Start()
     {
-       
+        // 测试同步加载
         GameObject obj1 = ABManager.GetInstance().LoadResource("model", "Cube") as GameObject;
         obj1.transform.position = -Vector3.up;
         GameObject obj2 = ABManager.GetInstance().LoadResource("model", "Cube", typeof(GameObject)) as GameObject;
-        obj2.transform.position = Vector3.up;
+        obj2.transform.position = -Vector3.down;
         GameObject obj3 = ABManager.GetInstance().LoadResource<GameObject>("model", "Cube");
-        obj3.transform.position = Vector3.left;
+        obj3.transform.position = -Vector3.left;
+
+
+        // 测试异步加载
+        ABManager.GetInstance().LoadResourceAsync("model", "Cube",
+            ob1 => { (ob1 as GameObject).transform.position = Vector3.up * 3; });
+        ABManager.GetInstance().LoadResourceAsync("model", "Cube", typeof(GameObject),
+            ob2 => { (ob2 as GameObject).transform.position = Vector3.down * 3; });
+        ABManager.GetInstance().LoadResourceAsync<GameObject>("model", "Cube",
+            ob3 => { ob3.transform.position = Vector3.right * 3; });
     }
 }
--- ABTest/Assets/AB-Resource-Load-Manager/ABManager.cs
+++ ABTest/Assets/AB-Resource-Load-Manager/ABManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 
 /// <summary>
@@ -37,6 +38,8 @@ public class ABManager : Singleton<ABManager>
 
     // 用字典存储加载过的AB包
     private Dictionary<string, AssetBundle> abDict = new Dictionary<string, AssetBundle>();
+
+    #region 同步加载
 
     /// <summary>
     /// 加载AB包
@@ -119,9 +122,95 @@ public class ABManager : Singleton<ABManager>
         return obj;
     }
 
-    // 异步加载
+    #endregion
 
-    // 单个包卸载
+    #region MyRegion
+
+    // 异步加载 AB包并没有使用异步加载 只是从ab包中加载资源时使用异步
+    // 提供给外部的方法，目的是启动协程
+
+    /// <summary>
+    /// 根据名字异步加载资源
+    /// </summary>
+    /// <param name="abName"></param>
+    /// <param name="resName"></param>
+    /// <param name="callBack"></param>
+    public void LoadResourceAsync(string abName, string resName, UnityAction<Object> callBack)
+    {
+        StartCoroutine(ReallyLoadRes(abName, resName, callBack));
+    }
+
+    private IEnumerator ReallyLoadRes(string abName, string resName, UnityAction<Object> callBack)
+    {
+        LoadAB(abName);
+        // 为了在外部使用方便，在加载资源的时候判断一下 资源是不是GameObject 实例化后返回给外部
+        AssetBundleRequest abr = abDict[abName].LoadAssetAsync(resName);
+        yield return abr;
+
+        // 异步加载结束后，通过委托传递给外部
+        if (abr.asset is GameObject)
+            callBack(Instantiate(abr.asset));
+        else
+            callBack(abr.asset);
+    }
+
+    /// <summary>
+    /// 根据Typo异步加载资源
+    /// </summary>
+    /// <param name="abName"></param>
+    /// <param name="resName"></param>
+    /// <param name="type"></param>
+    /// <param name="callBack"></param>
+    public void LoadResourceAsync(string abName, string resName, System.Type type, UnityAction<Object> callBack)
+    {
+        StartCoroutine(ReallyLoadRes(abName, resName, type, callBack));
+    }
+
+    private IEnumerator ReallyLoadRes(string abName, string resName, System.Type type, UnityAction<Object> callBack)
+    {
+        LoadAB(abName);
+        // 为了在外部使用方便，在加载资源的时候判断一下 资源是不是GameObject 实例化后返回给外部
+        AssetBundleRequest abr = abDict[abName].LoadAssetAsync(resName, type);
+        yield return abr;
+
+        // 异步加载结束后，通过委托传递给外部
+        if (abr.asset is GameObject)
+            callBack(Instantiate(abr.asset));
+        else
+            callBack(abr.asset);
+    }
+
+    /// <summary>
+    /// 根据泛型进行异步加载资源
+    /// </summary>
+    /// <param name="abName"></param>
+    /// <param name="resName"></param>
+    /// <param name="callBack"></param>
+    /// <typeparam name="T"></typeparam>
+    public void LoadResourceAsync<T>(string abName, string resName, UnityAction<T> callBack) where T : Object
+    {
+        StartCoroutine(ReallyLoadRes<T>(abName, resName, callBack));
+    }
+
+    private IEnumerator ReallyLoadRes<T>(string abName, string resName, UnityAction<T> callBack) where T : Object
+    {
+        LoadAB(abName);
+        // 为了在外部使用方便，在加载资源的时候判断一下 资源是不是GameObject 实例化后返回给外部
+        AssetBundleRequest abr = abDict[abName].LoadAssetAsync<T>(resName);
+        yield return abr;
+
+        // 异步加载结束后，通过委托传递给外部
+        if (abr.asset is GameObject)
+            callBack(Instantiate(abr.asset) as T);
+        else
+            callBack(abr.asset as T);
+    }
+
+    #endregion
+
+    #region 卸载AB包
+
+    /// 单个包卸载
     public void Unload(string abName)
     {
         if (abDict.ContainsKey(abName))
@@ -133,7 +222,7 @@ public class ABManager : Singleton<ABManager>
         }
     }
 
-    // 所有包的卸载
+    /// 所有包的卸载
     public void ClearAllAB()
     {
         AssetBundle.UnloadAllAssetBundles(false);
@@ -141,4 +230,6 @@ public class ABManager : Singleton<ABManager>
         mainAB = null;
         manifest = null;
     }
+
+    #endregion
 }
