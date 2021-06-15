5af4ec665607c1ff40e37388241bbd5db29dd656
wankcn <wankcn@icloud.com>
2021-06-15 11:43:23 +0800

    在Test.lua中定义函数（无参无返回，有参有返回，多返回，可变参数）

--- LearnXlua/Assets/Lua/Test.lua
+++ LearnXlua/Assets/Lua/Test.lua
@@ -9,3 +9,31 @@ testString = "123"
 
 -- 本地变量无法通过C# 调用
 local num = 2
+
+-- 函数的获取
+-- 无参无返回值
+testFun = function()
+	print("无参无返回值")
+end
+
+-- 有参又返回值
+testFun2 = function(a)
+	print("有参数有返回值")
+	return a+1
+end
+
+-- 多返回值
+testFun3 = function(a)
+	print("多返回值")
+	return 1,2,false,"123",a
+end
+
+-- 变长参数
+testFun4 = function(a,...)
+	print("变长参数")
+	print(a)
+	arg={...}
+	for k,v in pairs(arg) do
+		print(k,v)
+	end
+end
